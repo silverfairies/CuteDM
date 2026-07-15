9a57db643e7ce658d884257a4088ebed89706841
silverfairies <silver.fairies16@gmail.com>
2026-07-15 15:32:48 +0200

    plan, script directory parser

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,60 @@
-#![allow(unused)]
-use cursive::{Cursive, CursiveExt, View, direction::Direction, theme::{BaseColor, BorderStyle, Color, Palette, PaletteColor, Theme}, view::{Margins, Nameable}, views::{self, HideableView, LinearLayout, ListView, PaddedView, Panel, SelectView, TextView}};
+use std::{io::Error, path::PathBuf, process::Command};
 
-fn main() {
+use cursive::{
+    Cursive, CursiveExt,
+    theme::{BaseColor, BorderStyle, Color, Palette, PaletteColor, Theme},
+    view::{Margins, Nameable},
+    views::{self, HideableView, LinearLayout, PaddedView, Panel, SelectView, TextView},
+};
+
+use crate::mapper::Choice;
+
+mod mapper;
+
+fn main() -> Result<(), Error> {
+    /*
     let mut ui = Cursive::new();
 
-    let mut theme = Theme{ shadow : false, borders : BorderStyle::Outset, palette : Palette::terminal_default()};
-    theme.palette[PaletteColor::Background] = Color::Dark(BaseColor::Black);
-    theme.palette[PaletteColor::Primary] = Color::Dark(BaseColor::White);
-    theme.palette[PaletteColor::View] = Color::Dark(BaseColor::Black);
-    ui.set_theme(theme);
+    ui.set_theme(theme_constructor());
 
-    //ui.add_layer( TextView::new("Meow, mrrp!"));
-    //ui.add_layer(Panel::new(TextView::new("Mrryaoo!!")));
-    let mut list = SelectView::new()
+    let list = SelectView::new()
         .item("Mrrp", "Mrrp")
         .item("Nya", "Nya")
         .item("Meow", "Meow")
-
         .on_submit(|ui, choice: &str| {
             ui.set_user_data(choice.to_string());
-            ui.call_on_name("menu0", |menu_zero: &mut SelectView<&str>| menu_zero.disable() );
-            ui.call_on_name("menu", |menu_wrapper: &mut HideableView<PaddedView<SelectView<&str>>>| {
-                menu_wrapper.unhide();
-                //menu_wrapper.get_inner_mut().enable();
+            ui.call_on_name("menu0", |menu_zero: &mut SelectView<&str>| {
+                menu_zero.disable()
             });
-            ui.focus_name("menu");
-        }
-    ).with_name("menu0");
+            ui.call_on_name(
+                "menu",
+                |menu_wrapper: &mut HideableView<PaddedView<SelectView<&str>>>| {
+                    menu_wrapper.unhide();
+                    //menu_wrapper.get_inner_mut().enable();
+                },
+            );
+            let _ = ui.focus_name("menu");
+        })
+        .with_name("menu0");
 
     let list2 = SelectView::new()
         .item("blank", "")
         .item("period", ".")
         .item("exclamation mark", "!")
         .item("colon three", ":3")
-
         .on_submit(|ui, choice: &str| {
-            let mut output = ui.take_user_data().unwrap_or("error!".to_string()).to_string();
+            let mut output = ui
+                .take_user_data()
+                .unwrap_or("error!".to_string())
+                .to_string();
             output.push_str(choice);
             ui.set_user_data(output);
             ui.quit();
-        }
-    );
+        });
 
-    let list2wrap = HideableView::new(PaddedView::new(Margins::tb(1, 0), list2)).hidden().with_name("menu");
+    let list2wrap = HideableView::new(PaddedView::new(Margins::tb(1, 0), list2))
+        .hidden()
+        .with_name("menu");
 
     ui.add_layer(
         Panel::new(
@@ -50,16 +62,49 @@ fn main() {
                 .child(TextView::new("Meowrr"))
                 .child(Panel::new(TextView::new(":3")))
                 .child(list)
-                .child(list2wrap)
-        ).title("Meower")
+                .child(list2wrap),
+        )
+        .title("Meower"),
     );
     ui.add_global_callback('q', |ui| ui.quit());
 
     ui.run();
 
-    println!("{}", ui.user_data().unwrap_or(&mut "error ocured".to_string()))
+    let mut problem = "error occured".to_string();
+
+    let input = ui.user_data().unwrap_or(&mut problem);
+    //println!(
+    //    "{}",
+    //    &input
+    //);
+
+    let _ = Command::new("sh")
+        .args(["-c", format!("echo {}", input).as_str()])
+        .spawn();
+    */
+
+    println!(
+        "{:#?}",
+        Choice::read_tree(
+            PathBuf::from("/home/argentum/Documents/Projects/Rust/cutedm/tmp").read_dir()?
+        )?
+    );
+    Ok(())
+}
+
+#[allow(unused)]
+fn theme_constructor() -> Theme {
+    let mut theme = Theme {
+        shadow: false,
+        borders: BorderStyle::Outset,
+        palette: Palette::terminal_default(),
+    };
+    theme.palette[PaletteColor::Background] = Color::Dark(BaseColor::Black);
+    theme.palette[PaletteColor::Primary] = Color::Dark(BaseColor::White);
+    theme.palette[PaletteColor::View] = Color::Dark(BaseColor::Black);
+    theme
 }
 
 //struct Environment {
 //    output: String,
-//    global_layout: 
+//    global_layout:
---
+++ src/mapper.rs
@@ -0,0 +1,67 @@
+#![allow(unused)]
+
+use std::{
+    fs::{DirEntry, ReadDir},
+    io::Error,
+    iter::Peekable,
+    path::PathBuf,
+};
+
+#[derive(Debug)]
+pub struct Choice {
+    name: String,
+    path: PathBuf,
+    subchoices: Option<Vec<Choice>>,
+}
+
+impl Choice {
+    fn new(path: PathBuf) -> Self {
+        let name = path
+            .file_name()
+            .unwrap_or_default()
+            .to_str()
+            .unwrap_or("Invalid option")
+            .strip_suffix(".sh")
+            .unwrap_or("Invalid option")
+            .to_string();
+
+        let subchoices: Option<Vec<Choice>> =
+            if path.with_file_name(&name).exists() && path.with_file_name(&name).is_dir() {
+                if let Result::Ok(entries) = path.with_file_name(&name).read_dir() {
+                    Self::read_tree(entries).ok()
+                } else {
+                    None
+                }
+            } else {
+                None
+            };
+
+        Self {
+            name,
+            path,
+            subchoices,
+        }
+    }
+
+    pub fn read_tree(files: ReadDir) -> Result<Vec<Choice>, Error> {
+        let mut output: Vec<Choice> = Vec::new();
+        let mut files_filtered = files
+            .filter(|file| {
+                matches!(file,
+                Ok(entry)
+                if entry.path().is_file()
+                && entry.path().extension().unwrap_or_default().eq("sh")
+                )
+            })
+            .peekable();
+        if files_filtered.peek().is_none() {
+            Err(Error::other("Empty Directory"))
+        } else {
+            for script in files_filtered {
+                output.push(Choice::new(script?.path()));
+            }
+
+            Ok(output)
+        }
+    }
+}
